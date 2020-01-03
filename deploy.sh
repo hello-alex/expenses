41bd2afcbf655d201ed48fba694b0bb910a89874
Alex Zhang <alex.zhang@airbnb.com>
2020-01-03 15:43:39 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
-if [ $# -eq 0 ]
-  then
-    echo "Add a commit message"
-    return 1
-fi
+rm -rf ../website/expenses
+cp -a build/. ../website/expenses
 
-git add .
-git commit -m "$1"
-git push origin master
+# if [ $# -eq 0 ]
+#   then
+#     echo "Add a commit message"
+#     return 1
+# fi
 
-# expenses.chodounsky.net
-REACT_APP_SHEET_ID=18uwYwUAVw0H5bhszMgAORmvAN2APxAtJI3FB-XH7Dzk npm run build
-cp -a build/. production/
-cd production
-git add .
-git commit -m "$1"
-git push origin production
-cd ..
+# git add .
+# git commit -m "$1"
+# git push origin master
 
-# github pages
+# # expenses.chodounsky.net
+# REACT_APP_SHEET_ID=18uwYwUAVw0H5bhszMgAORmvAN2APxAtJI3FB-XH7Dzk npm run build
+# cp -a build/. production/
+# cd production
+# git add .
+# git commit -m "$1"
+# git push origin production
+# cd ..
 
-npm run build
-cp -a build/. demo/
-cd demo
-git add .
-git commit -m "$1"
-git push origin gh-pages
-cd ..
+# # github pages
+
+# npm run build
+# cp -a build/. demo/
+# cd demo
+# git add .
+# git commit -m "$1"
+# git push origin gh-pages
+# cd ..
